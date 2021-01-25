9a30acec005629af0b4c2a8a5aed5e629608ec36
94Cooper94 <LCAhearn94@gmail.com>
2021-01-25 17:21:11 -0600

    attempting to get the BTC tracker live on the homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import BTCTracker from './dashboard/BTCTracker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App />, 
+<BTCTracker />,
+document.getElementById('root'));
